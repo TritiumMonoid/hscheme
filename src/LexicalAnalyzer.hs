8247420e419fde456d3bb74f2ed1941b679b0f7e
TritiumMonoid <tritiummonoid@gmail.com>
2019-03-26 23:12:30 -0700

    add more complex types for LexycalAnalyzer

--- src/LexicalAnalyzer.hs
+++ src/LexicalAnalyzer.hs
@@ -3,7 +3,35 @@ module LexicalAnalyzer (tokens) where
 import Token
 import Text.Regex
 
-regexs :: [(String -> Token, Regex)]
+type TokenRegex = (String -> Token, Regex)
+
+data TokenMatch = TokenMatch (Token, String, String, String)
+instance Eq TokenMatch where
+    (==) (TokenMatch (_, b, m, _)) (TokenMatch (_, b', m', _)) =
+      length b >= length b' && length m >= length m'
+instance Ord TokenMatch where
+  compare first second
+    | first < second = LT
+    | first > second = GT
+    | otherwise = EQ
+  (<) (TokenMatch (_, b, m, _)) (TokenMatch (_, b', m', _)) =
+    length b < length b' && length m < length m'
+  (<=) (TokenMatch (_, b, m, _)) (TokenMatch (_, b', m', _)) =
+    length b <= length b' && length m <= length m'
+  (>) (TokenMatch (_, b, m, _)) (TokenMatch (_, b', m', _)) =
+    length b > length b' && length m > length m'
+  (>=) (TokenMatch (_, b, m, _)) (TokenMatch (_, b', m', _)) =
+    length b >= length b' && length m >= length m'
+  min first second
+    | first < second = first
+    | first > second = second
+    | otherwise = first
+  max first second
+    | first < second = second
+    | first > second = first
+    | otherwise = first
+
+regexs :: [TokenRegex]
 regexs = [ (const Comment, mkRegex ";.*$")
          , (const Comment, mkRegex "\\|\\#(.|\n)*?\\#\\|")
          , (const OpenParenthesis, mkRegex "(")
@@ -16,11 +44,17 @@ regexs = [ (const Comment, mkRegex ";.*$")
          , (Symbol, mkRegex "[^\\s()\\[\\]{}'\";]+")
          ]
 
-match :: String -> (String -> Token, Regex) -> Maybe (String, Token, String)
-match text (token, regex) = match' (matchRegexAll regex text)
-  where match' (Just (before, matched, after, _)) =
-          Just (before, token matched, after)
-        match' _ = Nothing
+match :: String -> TokenRegex -> Maybe TokenMatch
+match text (token, regex) = (matchRegexAll regex text) >>= (match')
+  where match' (before, matched, after, _) =
+          Just (TokenMatch (token matched, before, matched, after))
+
+matchNext :: [Maybe TokenMatch] -> Maybe TokenMatch
+matchNext = foldl maybeMax Nothing
+  where
+    maybeMax (Just first) (Just second) = Just (max first second)
+    maybeMax first Nothing = first
+    maybeMax Nothing second = second
 
 tokens :: String -> [Token]
 tokens text = []
